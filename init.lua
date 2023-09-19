0f2e3458d31bebc7de7d1f654ffb104b8eac4d64
takahito.sudo.nd <Takahito.Sudo@nttdata.com>
2023-09-19 22:25:51 +0900

    :wrench: Add config files to develop

---
+++ init.lua
@@ -0,0 +1,260 @@
+-- common settings
+local opt = vim.opt
+opt.number = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+
+-- package manager
+require("packer").startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'windwp/nvim-ts-autotag'
+  use 'mattn/emmet-vim'
+  use {
+	'windwp/nvim-autopairs',
+	config = function() require('nvim-autopairs').setup {} end
+  }
+
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/vim-vsnip"
+  -- use "hrsh7th/cmp-path"
+  -- use "hrsh7th/cmp-buffer"
+  -- use "hrsh7th/cmp-cmdline"
+
+  -- color scheme
+  use 'kristijanhusak/vim-hybrid-material'
+  use 'folke/tokyonight.nvim'
+
+  use 'nvim-tree/nvim-web-devicons'
+  use {
+	  'nvim-lualine/lualine.nvim',
+	  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use 'christoomey/vim-tmux-navigator'
+ 
+  use 'scrooloose/nerdtree'
+
+  use 'ryanoasis/vim-devicons'
+ 
+  use 'nvim-treesitter/nvim-treesitter'
+
+  -- git 
+  use 'airblade/vim-gitgutter'
+
+  use {
+	  'numToStr/Comment.nvim',
+	  config = function()
+		  require('Comment').setup()
+	  end
+  }
+
+  use 'voldikss/vim-floaterm'
+
+end)
+
+-- 1. LSP Sever management
+require('mason').setup()
+require('mason-lspconfig').setup_handlers({ function(server)
+  local opt = {
+    -- -- Function executed when the LSP server startup
+    -- on_attach = function(client, bufnr)
+    --   local opts = { noremap=true, silent=true }
+    --   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    --   vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
+    -- end,
+    capabilities = require('cmp_nvim_lsp').default_capabilities(
+      vim.lsp.protocol.make_client_capabilities()
+    )
+  }
+  require('lspconfig')[server].setup(opt)
+end })
+
+-- 2. build-in LSP function
+-- keyboard shortcut
+vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.format()<CR>')
+vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+vim.keymap.set('n', '<Tab>', '<cmd>bnext<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<S-Tab>', '<cmd>bprevious<CR>', {noremap = true, silent = true })
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- LSP handlers
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true }
+)
+-- Reference highlight
+vim.cmd [[
+set updatetime=500
+highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+augroup lsp_document_highlight
+  autocmd!
+  autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
+  autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+augroup END
+]]
+
+-- 3. completion (hrsh7th/nvim-cmp)
+local cmp = require("cmp")
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    -- { name = "buffer" },
+    -- { name = "path" },
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ['<C-l>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+  }),
+  experimental = {
+    ghost_text = true,
+  },
+})
+-- cmp.setup.cmdline('/', {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = {
+--     { name = 'buffer' }
+--   }
+-- })
+-- cmp.setup.cmdline(":", {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = {
+--     { name = "path" },
+--     { name = "cmdline" },
+--   },
+-- })
+--
+
+-- 4. colorscheme
+vim.cmd [[
+colorscheme tokyonight-storm
+]]
+
+-- 5. statusline
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'material',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    },
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'%f'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {
+    lualine_a = {'buffers'},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+
+-- templates
+vim.cmd [[
+autocmd BufNewFile ~/Sandbox/Atcoder/*/*.cpp :0r ~/.config/nvim/templates/atcoder.cpp
+autocmd BufNewFile ~/Sandbox/ant/*.cpp :0r ~/.config/nvim/templates/atcoder.cpp
+autocmd BufNewFile ~/Sandbox/paiza/*.go :0r ~/.config/nvim/templates/paiza.go
+autocmd BUfNewFile ~/Workspace/oj/*.go :0r ~/.config/nvim/templates/paiza.go
+]]
+
+-- nerdtree
+vim.keymap.set('n', '<Leader>n', '<cmd>NERDTreeFocus<CR>')
+vim.keymap.set('n', '<C-n>', '<cmd>NERDTree<CR>')
+vim.keymap.set('n', '<C-t>', '<cmd>NERDTreeToggle<CR>')
+vim.keymap.set('n', '<C-f>', '<cmd>NERDTreeFind<CR>')
+vim.g.webdevicons_enable_nerdtree = 1
+
+require('Comment').setup()
+
+-- null_ls
+local status, null_ls = pcall(require, "null-ls")
+if (not status) then return end 
+
+null_ls.setup({
+	source = {
+		null_ls.builtins.diagnostics.eslint_d.with({
+			diagnostics_format = '[eslint] #{m}\n(#{c})'
+		}),
+		null_ls.builtins.diagnostics.fish
+	}
+})
+
+local status, prettier = pcall(require, "prettier")
+if (not status) then return end
+
+prettier.setup {
+  bin = 'prettierd',
+  filetypes = {
+    "css",
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "json",
+    "scss",
+    "less"
+  }
+}
+
